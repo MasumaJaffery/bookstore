3ccf522c76ea03e58341672b9c66680b45fad49e
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-22 09:13:05 -0700

    Refactor All

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable linebreak-style */
 import React, { useState } from 'react';
-import BookList from './Booklist';
-/* eslint-disable import/extensions */
+import BookList from './BookList';
 /* Book Already imported in Booklist so that's why i don't import it here! */
 
 function AddBook() {

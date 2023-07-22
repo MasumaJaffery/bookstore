6f320538152ee70f58a9686d085416054198722b
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-22 03:32:21 -0700

    refactor

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,27 +1,26 @@
 /* eslint-disable linebreak-style */
 import React, { useState } from 'react';
 import Booklist from './Booklist';
-/* Book Already imported in Booklist so that's why i don't import it here! */
 
 const AddBook = () => {
   const [title, setTitle] = useState('');
-  const [category, setCategory] = useState('');
+  const [author, setAuthor] = useState('');
   const [books, setBooks] = useState([]);
 
   const handleAddBook = (e) => {
     e.preventDefault();
-    if (title.trim() === '' || category.trim() === '') {
+    if (title.trim() === '' || author.trim() === '') {
       return;
     }
     const newBook = {
       id: Date.now(),
       title,
-      category,
+      author,
     };
 
     setBooks((prevBooks) => [...prevBooks, newBook]);
     setTitle('');
-    setCategory('');
+    setAuthor('');
   };
 
   return (
@@ -40,9 +39,9 @@ const AddBook = () => {
             type="text"
             id="category"
             name="category"
-            placeholder="Category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
+            placeholder="Author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
           />
           <button type="submit">Add Book</button>
         </form>

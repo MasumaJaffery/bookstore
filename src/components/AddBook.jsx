aab73786ec35d3a5334491d390734c97a3ab4921
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-22 02:58:33 -0700

    Add

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
-import React, { useState } from 'react';
-import BookList from './Booklist';
 /* Book Already imported in Booklist so that's why i don't import it here! */
+import React, { useState } from 'react';
+import Booklist from './Booklist';
 
 const AddBook = () => {
   const [title, setTitle] = useState('');
@@ -47,7 +47,7 @@ const AddBook = () => {
           <button type="submit">Add Book</button>
         </form>
       </div>
-      <BookList
+      <Booklist
         books={books}
         // eslint-disable-next-line max-len
         onRemove={(bookId) => setBooks((prevBooks) => prevBooks.filter((book) => book.id !== bookId))}

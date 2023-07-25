2623d39e343912b961a6127d861b893867d54032
Syeda Masuma Fatima <masumajaffery1@gmail.com>
2023-07-25 11:45:15 -0700

    Add Books Data in Slice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -2,7 +2,27 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const bookslice = createSlice({
   name: 'book',
-  initialState: [],
+  initialState:
+  [
+    {
+      item_id: 'item1',
+      title: 'The Great Gatsby',
+      author: 'John Smith',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item2',
+      title: 'Anna Karenina',
+      author: 'Leo Tolstoy',
+      category: 'Fiction',
+    },
+    {
+      item_id: 'item3',
+      title: 'The Selfish Gene',
+      author: 'Richard Dawkins',
+      category: 'Nonfiction',
+    },
+  ],
   reducers: {
     addBook(state, action) {
       const NewBook = action.payload;
